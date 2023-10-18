3b7c0ed4a910ab4ca0f576bf94c2e755125a7d82
CarlKaste <117178619+CarlKaste@users.noreply.github.com>
2023-10-18 22:27:03 +0200

    Timeline for the first scene is complete!

--- ARES-Digital/Assets/Scripts/SpawnExplotion.cs
+++ ARES-Digital/Assets/Scripts/SpawnExplotion.cs
@@ -6,17 +6,41 @@ public class SpawnExplotion : MonoBehaviour
 {
     [SerializeField] private GameObject explotionPrefab;
     [SerializeField] private GameObject fireWallSphereTrigger;
+
     private GameObject[] effects = new GameObject[3];
+    private PlaySteps steps;
+    private EventManager eventManager;
+
+    public bool hasFiredOnes = false;
+
+    private void Start()
+    {
+        steps = GameObject.FindGameObjectWithTag("NarrativeStoryManager").GetComponent<PlaySteps>();
+        eventManager = GameObject.FindGameObjectWithTag("EventManager").GetComponent<EventManager>();
+    }
 
     public void SpawnExplotionEffect()
     {
+        if(hasFiredOnes == false)
+        {
+            hasFiredOnes = true;
+            StartCoroutine(DelayEnemySpawnAfterUse());
+        }
+
         effects[0] = Instantiate(explotionPrefab, this.transform.position, this.transform.rotation);
         StartCoroutine(DespawnExplotionEffect());
     }
 
+    IEnumerator DelayEnemySpawnAfterUse()
+    {
+        steps.PlayStepIndex(5);
+        yield return new WaitForSeconds(10);
+        eventManager.ActivateSecondEnemyWave();
+    }
+
     IEnumerator DespawnExplotionEffect()
     {
-        yield return new WaitForSeconds(2);
+        yield return new WaitForSeconds(1);
         fireWallSphereTrigger.SetActive(false);
 
         foreach (GameObject e in effects)
