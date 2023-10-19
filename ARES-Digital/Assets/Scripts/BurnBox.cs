55c82d19a38bbe52b32dd7fa590cb3d13ec55cf6
CarlKaste <117178619+CarlKaste@users.noreply.github.com>
2023-10-19 23:38:32 +0200

    Fixed the last scene in the timeline
    Now the second scene is complete with the events in the timeline

--- ARES-Digital/Assets/Scripts/BurnBox.cs
+++ ARES-Digital/Assets/Scripts/BurnBox.cs
@@ -5,21 +5,22 @@ using UnityEngine;
 public class BurnBox : MonoBehaviour
 {
     [SerializeField] private GameObject corpseFireEffectPrefab;
+    [SerializeField] private Vector3 fireOffset;
 
     private Transform boss;
     private PlaySteps steps;
 
     private void Start()
     {
-        boss = gameObject.GetComponentInParent<Transform>();
+        boss = GameObject.FindGameObjectWithTag("Boss").GetComponent<Transform>();
         steps = GameObject.FindGameObjectWithTag("NarrativeStoryManager").GetComponent<PlaySteps>();
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("Explosion"))
+        if (other.gameObject.CompareTag("PlayerHand"))
         {
-            Instantiate(corpseFireEffectPrefab, boss.transform);
+            Instantiate(corpseFireEffectPrefab, boss.position + fireOffset, boss.rotation);
             steps.PlayStepIndex(3);
         }
     }
